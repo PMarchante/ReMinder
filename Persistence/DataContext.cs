66410e911bf0bb1efed1ac3c077afd5cdbf61dc7
Pedro Marchante <pmarc560@gmail.com>
2019-09-30 19:33:28 -0400

    initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Domain;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {}
+        DbSet<Reminder> Reminders {get; set;}
+    }
+}
